889b5203846b97f9aa6fa35e20c81480a54bde61
ZimonKuhs <zmk.professional@gmail.com>
2021-07-22 13:12:31 +0200

    feat(JS): Add pointless sandboxes.

---
+++ src/buffers.js
@@ -0,0 +1,24 @@
+/*
+ *  Buffer sandboxing.
+ *
+ *  N.B. Opting to use new buffer logic as old behavior was deprecated in Node.js 10.
+ *
+ *  @author Zimon Kuhs
+ *  @date   2021-07-22
+ *  @see    https://nodesource.com/blog/understanding-the-buffer-deprecation-in-node-js-10/
+ */
+
+let buffer = Buffer.from(new ArrayBuffer(256))
+let buffer2 = Buffer.from(new ArrayBuffer(256))
+
+buffer.write("I am not sure why we're doing this yet.\n")
+buffer2.write("Not now either, really.\n")
+buffer2.copy(buffer)
+
+console.log(buffer.slice(0, 3).toString())
+console.log(buffer2.toString())
+console.log(buffer.length)
+
+function newBuffer(octets) {
+    return Buffer.from(new ArrayBuffer(octets))
+}
---
+++ src/events.js
@@ -0,0 +1,32 @@
+/*
+ *  Event sandboxing.
+ *
+ *  @author Zimon Kuhs
+ *  @date   2021-07-22
+ */
+
+import events from "events"
+
+const eventEmitter = new events.EventEmitter()
+
+const listener1 = function listener1() {
+    console.log("ONEONE")
+}
+const listener2 = function listener2() {
+    console.log("Dos amigos")
+}
+
+eventEmitter.addListener("connection", listener1)
+eventEmitter.on("connection", listener2)
+
+eventEmitter.emit("connection")
+
+eventEmitter.removeListener("connection", listener1)
+
+eventEmitter.emit("connection")
+
+console.log("FIN.")
+
+function listenerMessage(number) {
+    console.log("We have " + number + " listeners doing their stuff.")
+}
