b8cc1c07480c200c4225e147c2f2fd527c5420cc
ZimonKuhs <zmk.professional@gmail.com>
2021-07-22 13:28:27 +0200

    feat(JS): Add base file for stream sandboxing.

---
+++ src/streams.js
@@ -0,0 +1,27 @@
+/*
+ *  Stream sandboxing.
+ *
+ *  @author Zimon Kuhs
+ *  @date   2021-07-22
+ */
+
+import fs from "fs"
+let data = ""
+
+let readerStream = fs.createReadStream("input.txt")
+
+readerStream.setEncoding("UTF8")
+
+readerStream.on("data", function (chunk) {
+    data += chunk
+})
+
+readerStream.on("end", function () {
+    console.log(data)
+})
+
+readerStream.on("error", function (err) {
+    console.log(err.stack)
+})
+
+console.log("Program Ended")
