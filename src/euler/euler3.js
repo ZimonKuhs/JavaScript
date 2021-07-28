ea8a78878798fafebe5507d72730fe5025988cb8
ZimonKuhs <zmk.professional@gmail.com>
2021-07-28 11:38:42 +0200

    feat(Euler): Adding skeleton for problem #3.

---
+++ src/euler/euler3.js
@@ -0,0 +1,18 @@
+/*
+ *  'Largest Prime Factor'
+ *
+ *  https://projecteuler.net/problem=3
+ *
+ *  @author:    Zimon Kuhs
+ *  @date:      2021-07-28
+ *  @solution:  6857
+ */
+
+/**
+ *  Solves the problem of finding the largest prime factor of 600851475143.
+ *
+ *  @returns The solution to the specified problem.
+ */
+export default function solve() {
+    return undefined
+}
--- test/euler/euler.test.js
+++ test/euler/euler.test.js
@@ -1,6 +1,8 @@
 /**
  *  Tests that all euler problems still return their correct solutions.
  *
+ *  TODO:   Add test(s) for euler module main.
+ *
  *  @author Zimon Kuhs
  *  @date   2021-07-28
  *  @see    /src/euler/
@@ -8,11 +10,15 @@
 
 import assert from "assert"
 import euler2 from "../../src/euler/euler2.js"
+import euler3 from "../../src/euler/euler3.js"
 
 const assertEqual = assert.strictEqual
 
 describe("Euler problems.", () => {
     it("#2.", () => {
         assertEqual(4613732, euler2())
+    })
+    it("#3.", () => {
+        assertEqual(6857, euler3())
     })
 })
