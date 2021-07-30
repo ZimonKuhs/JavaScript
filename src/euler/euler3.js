51e201f019643f944797f9f8e05536dbb920486b
Zimon Kuhs <zmk.professional@gmail.com>
2021-07-30 20:05:40 +0200

    chore(Javascript): Do the big workspace format.

--- src/euler/euler3.js
+++ src/euler/euler3.js
@@ -8,11 +8,33 @@
  *  @solution:  6857
  */
 
+import utility from "../utility/index.js"
+
 /**
  *  Solves the problem of finding the largest prime factor of 600851475143.
  *
  *  @returns The solution to the specified problem.
  */
 export default function solve() {
-    return undefined
+    return Math.max(sieve(Math.sqrt(600851475143)))
+}
+
+function sieve(number) {
+    if (number <= 1) {
+        return []
+    }
+
+    const sieve = utility.filledArray(true, number)
+
+    for (let i = 2; i < Math.sqrt(number); ++i) {
+        if (!sieve[i]) {
+            continue
+        }
+
+        for (let j = i * i; j < number; j += i) {
+            sieve[j] = false
+        }
+    }
+
+    return sieve.map.reduce((highest, current) => (current > highest ? current : highest))
 }
--- src/utility/index.js
+++ src/utility/index.js
@@ -1,10 +1,13 @@
 /*
  *  Utility function library.
  *
+ *  TODO: Should maybe be split into sub-modules that are imported specifically.
+ *
  *  @author:    Zimon Kuhs
  *  @date:      2021-07-27
  */
 
+import { filledArray } from "./array.js"
 import { importFolder } from "./import.js"
 
-export default { importFolder }
+export default { filledArray, importFolder }
